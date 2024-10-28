0ee1fa5185f67dfb2d6a2ddcd09468af1107aa7e
Ian Clavel <ianclavel1991@gmail.com>
2024-10-29 00:44:43 +0800

    dependencies

--- lt/app/page.tsx
+++ lt/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -96,6 +97,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <Button />
     </div>
   );
 }
