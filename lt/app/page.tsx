23cf8d43c757477f81036c4d5c4d817da44265da
Ian Clavel <ianclavel1991@gmail.com>
2024-11-01 00:24:37 +0800

    feat: installed Sidebar from shadcn

--- lt/app/page.tsx
+++ lt/app/page.tsx
@@ -1,14 +1,8 @@
 import { FloatingDockDemo } from "@/components/navbar/floatingDock";
 
-import Terra from "@/public/terrarium.jpg";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
-      <header className="absolute ">
-        <Image src={Terra} alt="Terra BG" className="bg-cover bg-center" />
-      </header>
       <nav>
         <FloatingDockDemo />
       </nav>
