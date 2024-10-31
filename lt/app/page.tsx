9d470a053b8fc42aa01a6cbd60d46459bfea7629
Ian Clavel <ianclavel1991@gmail.com>
2024-10-31 23:27:50 +0800

    added terrarium.jpg to be used as background image

--- lt/app/page.tsx
+++ lt/app/page.tsx
@@ -1,11 +1,15 @@
 import { FloatingDockDemo } from "@/components/navbar/floatingDock";
 
+import Terra from "@/public/terrarium.jpg";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <div>
+      <div className="bg-[url('terrarium.jpg)]"></div>
+      <nav>
         <FloatingDockDemo />
-      </div>
+      </nav>
     </>
   );
 }
